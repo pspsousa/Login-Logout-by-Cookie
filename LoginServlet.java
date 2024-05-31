01042d46d40119dae02fb4b4a5d840005a520ade
Patricia Sousa <111404527+pspsousa@users.noreply.github.com>
2024-05-31 15:23:21 -0300

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+      
+        Cookie sessionCookie = new Cookie("user_session", username);
+        sessionCookie.setMaxAge(3600); 
+        response.addCookie(sessionCookie);
+        response.sendRedirect("profile");
+    }
+}
